d23a5568cf751b47a745897bd728d9d5ee32ca0a
Nick B <nicholas.balestrieri@gmail.com>
2019-12-02 18:26:20 -0500

    Server handles POST request with error handling

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Hi</title></head>");
+    res.write("<body><form action='/message' method='POST'>");
+    res.write(
+      "<input type='text' name='formContent'><button type='submit'>Send</button>"
+    );
+    res.write("</form></body>");
+    res.write("</html>");
+
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", data => {
+      body.push(data);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      fs.writeFile(
+        "message.txt",
+        `${new Date()}: ${parsedBody.split("=")[1]}`,
+        err => {
+          if (err) throw Error(err);
+          res.writeHead(302, { Location: "/" });
+          return res.end();
+        }
+      );
+    });
+  }
   res.setHeader("Content-Type", "text'html");
 
   res.write("<html>");
