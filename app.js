bee1646c3aa7d890fce7f50db05fc2496bc27f52
Nick B <nicholas.balestrieri@gmail.com>
2019-12-02 17:37:38 -0500

    Amend server to write HTML in multiple lines

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("Content-Type", "text'html");
+
+  res.write("<html>");
+  res.write("<head><title>Hi</title></head>");
+  res.write(`I appreciate the ${req.method} request`);
+  res.write("</html>");
+
+  res.end();
 });
 
 server.listen(5000);
